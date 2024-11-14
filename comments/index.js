56d80df0758603b3a0f8a1ca9975563565725810
YashTike <yashtike.usa@gmail.com>
2024-11-13 22:37:44 -0800

    1. Updated Comments service to emit "CommentUpdated" event every time it receives a "CommentModerated" event from the Moderation service. This event reflects the change in moderation status for a comment
    2. Updated Query service to handle "CommentUpdated" events and reflect the updated status and content of the moderated comment in local store.

--- comments/index.js
+++ comments/index.js
@@ -40,8 +40,31 @@ app.post('/posts/:id/comments', async (req, res) => {
     res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
     console.log('Received Event', req.body.type);
+
+    if(req.body.type === 'CommentModerated') {
+        const { id, postId, status, content } = req.body.data;
+
+        const comments = commentsByPostId[postId];
+
+        const comment = comments.find(comment => {
+            return comment.id === id;
+        });
+
+        comment.status = status;
+
+        //Send event to event-bus
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentUpdated',
+            data: {
+                id,
+                postId,
+                status,
+                content
+            }
+        });
+    }
 
     res.send({});
 });
